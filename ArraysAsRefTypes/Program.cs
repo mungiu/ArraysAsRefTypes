040e8af0d287034038cdc871bc6c31b2c18d6f87
Andrei Mungiu <andrei_mungiu@yahoo.com>
2017-08-18 11:25:26 +0200

    Tutorial checkpoint reached

--- ArraysAsRefTypes/Program.cs
+++ ArraysAsRefTypes/Program.cs
@@ -10,6 +10,13 @@ namespace ArraysAsRefTypes
     {
         static void Main(string[] args)
         {
+            int[] x1 = { 1, 4, 9, 16 };
+            var x2 = x1;
+
+            int[] x3 = { 1, 4, 9, 16 };
+
+            Console.WriteLine(string.Format($"x1==x2 is {x1 == x2}"));
+            Console.WriteLine(string.Format($"x1==x3 is {x1 == x3}"));
         }
     }
 }
